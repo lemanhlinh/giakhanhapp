41b37d4fc384111989a7bccc2507a8c150cf4b30
unknown <lemanhlinh209@gmail.com>
2023-12-26 11:00:34 +0700

    f

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    port: 8081, // Số port mong muốn
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
